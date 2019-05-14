ef2fb6cdd4400e30552c5c659d532a83fdd3459d
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-14 12:57:13 +0800

    Update list.h

--- rider/list.h
+++ rider/list.h
@@ -1,79 +1,64 @@
 #ifndef LIST_H
 #defien LIST_H
 
-template<typename T>
 class ListNode{
-    T data;
-    ListNode<T> *pred,*next;
+    int x,y;
+    int t;
+    ListNode *pred,*next;
     ListNode(){}
-    ListNode(T x,ListNode<T>* p=nullptr,ListNode<T>* n=nullptr): data(x)),pred(p),next(n) {}
+    ListNode(int x,int y,int t,ListNode* p=nullptr,ListNode* n=nullptr): x(x),y(y),t(t),pred(p),next(n) {}
 
-    ListNode<T>* insertAsPred (T const& e);
-	ListNode<T>* insertAssucc (T const& e); 
+    ListNode* insertAsPred (const int& x,const int& y,const int& t);
+	ListNode* insertAssucc (const int& x,const int& y,const int& t); 
 }
 
-template<typename T> listNode<T>* listNode<T>::insertAsPred(const T& e)
+ListNode* ListNode::insertAsPred(const int& x,const int& y,const int& t)
 {
-	listNode<T> *p = new listNode<T>(e, pred, this);    //更新4个指针的指向
-	pred->succ = p;
+	ListNode *p = new ListNode(x,y,t, pred, this);    //更新4个指针的指向
+	pred->next = p;
 	pred = p;
 	return p;
 }
 
-template<typename T> listNode<T>* listNode<T>::insertAsSucc(const T& e)
+ListNode* listNode::insertAsSucc(const int& x,const int& y,const int& t)
 {
-	listNode<T> *p = new listNode<T>(e, this, succ);
-	succ->pred = p;
+	ListNode *p = new ListNode(x,y,t, this, next);
+	succ->next = p;
 	succ = p;
 	return p;
 }
 
-template <typename T> class List{
+class List{
   	private:
   	 	int _size; 
-  	 	ListNode<T>* header,trailer;
+  	 	ListNode* header,trailer;
   	protected:
   	 	void init();
   	 	int clear();
-  	 	void copyNodes (ListNode<T>* ,int );
   	 	
  	public:
  	 	List() { init();}
-		List(List<T> const& L)；
-		List(List<T>,Rank r,int n); 
-		List(ListNode<T>* p,int n);
+		List(List const& L)；
+
 
 	 	~List();
 		 Rank size() const { return _size; }
 		 bool empty() const { return _size <= 0 ;}
-		 T& operator[] (Rank r) const; 
-		 ListNode<T>* first() const { return header->succ ;} 
-		 ListNode<T>* last() const { return trailer->pred ;}
-		 bool vaild (ListNode<T>* p)
+		 //int& operator[] (Rank r) const; 
+		 ListNode* first() const { return header->next ;} 
+		 ListNode* last() const { return trailer->pred ;}
+		 bool vaild (ListNode* p)
 		 {
 		     return p&&(trailer !=p )&&( header !=p) ;
 		 }
 	
 
-		void InsertAsf(T const& e);
-		void InsertAsl(T const& e);
-		ListNode<T>* InsertAsf(ListNode<T>* ,T const& e);
-		ListNode<T>* InsertAsl(ListNode<T>* ,T const& e); 
+		void InsertAsf(int const& e);
+		void InsertAsl(int const& e);
+		ListNode* InsertAsf(ListNode* ,int const& e);
+		ListNode* InsertAsl(ListNode* ,int const& e); 
 	  
 	    void remove(Rank r);
-	    void removeRange(Rank lo,Rank hi);
-	   
-	    void exchange(Rank r, T const& e);
-	  
-	    ListNode<T>* Rangesort(Rank lo,Rank hi);
-	    ListNode<T>* sort();
-	   
-	    ListNode<T>* deduplicated(Rank lo,Rank hi);
-	    ListNode<T>* Unique(Rank lo,Rank hi);
-	  
-	   	ListNode<T>* find(T const& e ,Rank lo, Rank hi); 
-	
-	    reverse();
 	  	 
 };
 

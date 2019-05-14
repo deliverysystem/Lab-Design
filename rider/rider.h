7b00265cf2eaedf18653e5d2c12e114cac8bf975
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-14 12:16:39 +0800

    Update rider.h

--- rider/rider.h
+++ rider/rider.h
@@ -3,26 +3,31 @@
 #include "tools.h"
 #include "point.h"
 #include "menu.h"
-
-using namespace std;
+#include "list.h"
 
 class Rider{
   private:
+    int exist;
     int x,y;
-    int t1;
-    List<Menu> waitlist;
-    int finishednum;
-    int unfinishednum;
-    int total;
+    Menu* waitlist;
+    List Path;
+    int receive;//骑手的接单数
+    int achieve;//骑手的完成数
+    int overtime;//骑手超时数
+
     //运动范围;
 
   public:
+    Point A(int x,int y):x(x),y(y);
     Rider() {}
     Rider(int x,int y):x(x),y(y);
-    void CalculatePath();
+    void CalculatePath(Menu* newlist);
+    int Manhatten(Point x,Point y);
+    void GeneratePath(Point A,int id);
     void PrintState();
+    int AllThingsDone();//返回送完最后一单的时间
     ~Rider() {}
-
+    friend Menu;
 }
 
 #endif //RIDER_H

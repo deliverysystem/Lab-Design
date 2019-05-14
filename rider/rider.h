8da4c9aa28da9c25768f78d734e6f24e6ae08cde
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-14 20:30:29 +0800

    Update rider.h

--- rider/rider.h
+++ rider/rider.h
@@ -41,32 +41,32 @@ class List{
   	 	
  	public:
  	 	List() { init();}
-    void copyNodes(ListNode<T>* p, int n) //这个是为了下一个函数服务的
+    		void copyNodes(ListNode<T>* p, int n) //这个是为了下一个函数服务的
 		List(List const& L);//构造链表时可以直接复制一个链表到另一个链表里面
     
 
 	 	~List();
 		 int size() const { return _size; } //返回链表的大小 调用的时候  len=Path.size()
 		 bool empty() const { return _size <= 0 ;}//链表是否为空, 注意 链表的header 和 tailer 不算在size里
-     ListNode* first() const {
-        return _size == 0 ? NULL : header->next;
-    }
-     ListNode* last() const {
-        return _size == 0 ? NULL : trailer->pred;
-    }
+     		ListNode* first() const {
+        		return _size == 0 ? NULL : header->next;
+    		}
+     		ListNode* last() const {
+        		return _size == 0 ? NULL : trailer->pred;
+    		}
 
-    List& operator =(const List& A)//赋值运算符重载  例如 A是一个List B是一个List  A=B 就是把B直接赋值给A
-    {
-      this._size = A.size();
-      ListNode* temp = A.first();
-      while(temp!=A.last()){
-          this.InsertAsl(temp);
-      }
-      return *this;
-    }
+		List& operator =(const List& A)//赋值运算符重载  例如 A是一个List B是一个List  A=B 就是把B直接赋值给A
+		{
+		      this._size = A.size();
+		      ListNode* temp = A.first();
+		      while(temp!=A.last()){
+			  this.InsertAsl(temp);
+		      }
+		      return *this;
+		}
 
-		void InsertAsl(ListNode* p);//插入到最后面 
-	  void remove(ListNode* p);// 删除
+	       void InsertAsl(ListNode* p);//插入到最后面 
+	       void remove(ListNode* p);// 删除
 	  	 
 };
 
@@ -88,7 +88,7 @@ void copyNodes(ListNode* p, int n) { //O(n)
         }
 }
 
-List(List const& L){  
+List::List(List const& L){  
         copyNodes( L.first(), L._size ); 
 }
 
@@ -138,7 +138,7 @@ class Rider{
     int CalculatePath(Menu* newmenu);//试算送完最后一单的时间
     int Manhatten(Point x,Point y);
     void GeneratePath(Point A,int id);
-    void AddTOWaitlist(Menu* newmenu);
+    void AddTOWaitlist(Menu* newmenu);//将新的订单添加进waitlist
     ~Rider() {}
     friend class Menu;
     friend class List;

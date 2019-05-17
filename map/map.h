8e0a635f5871c4d07f27cf842f03179f1b083f0d
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-17 23:14:23 +0800

    Update map.h

--- map/map.h
+++ map/map.h
@@ -1,24 +1,25 @@
 #ifndef MAP_H
 #define MAP_H
 #include "point.h"
+#include "tools.h" 
 #include <iostream>
-using namespace std;
 
 class Map{
-   public:
-	void init(){
-    for(x=0;x<60;x+=6)
-     for(y=0;y<80;y+=8)
+	public:
+    void init(){
+    for(x=0;x<=32;x+=4)			
+     for(y=0;y<=64;y+=8)
        {
            point c(x,y);
            if(Graph[x][y])
               c.PrintCanteen();
            else 
               c.PrintResidence();
-       }
+       } 
     }
+    private:
      int x,y;
-     int Graph[60][60];//ÓÃÓÚ¼ÇÂ¼Ä³¸öÎ»ÖÃÊÇ²ÍÌü»¹ÊÇ×¡Ëù ²ÍÌüÎª1,×¡ËùÎª0
+     int Graph[60][60];//ç”¨äºŽè®°å½•æŸä¸ªä½ç½®æ˜¯é¤åŽ…è¿˜æ˜¯ä½æ‰€ é¤åŽ…ä¸º1,ä½æ‰€ä¸º0
 };
 
 #endif // MAP_H

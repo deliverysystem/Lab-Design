a0b5d3db75277f90d39e7af36b8a7c7d07c18189
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-17 20:55:18 +0800

    Update point.cpp

--- point/point.cpp
+++ point/point.cpp
@@ -2,29 +2,33 @@
 #include "tools.h" 
 #include <cstdio> 
 #include <iostream>
-
+using namespace std;
 void point::clear(){
-	setCursorPosition(x,y);
+	SetCursorPosition(x,y);
 	std::cout<<" "; 
 };
 
 void point::PrintRider(){
-	setCursorPosition(x,y);
+	SetCursorPosition(x,y);
 	std::cout<<"*";
 };
 
 void point::PrintCanteen(){
-	setCursorPosition(x,y);
+	SetCursorPosition(x,y);
 	std::cout<<"------"<<endl;
-	std::cout<<"|餐厅|"<<endl;
-	srd::cout<<"------";
-	setCurSorPosition(x,y);
+	SetCursorPosition(x+1,y);
+	std::cout<<"|    |"<<endl;
+	SetCursorPosition(x+2,y);
+	std::cout<<"------";
+	SetCursorPosition(x,y);
 }
 
 void point::PrintResidence(){
-	setCursorPosition(x,y);
+	SetCursorPosition(x,y);
 	std::cout<<"------"<<endl;
-	std::cout<<"|住所|"<<endl;
-	srd::cout<<"------";
-	setCurSorPosition(x,y);
+	SetCursorPosition(x+1,y);
+	std::cout<<"|    |"<<endl;
+	SetCursorPosition(x+2,y);
+	std::cout<<"------";
+	SetCursorPosition(x,y);
 }

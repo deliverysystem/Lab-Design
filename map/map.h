a3ecc5c21d3cb1352d7fce48ad929ba2af2d5150
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-18 21:04:40 +0800

    Update map.h

--- map/map.h
+++ map/map.h
@@ -1,13 +1,13 @@
 #ifndef MAP_H
 #define MAP_H
 #include "point.h"
-#include "tools.h" 
 #include <iostream>
+using namespace std;
 
 class Map{
-	public:
-    void init(){
-    for(x=0;x<=32;x+=4)			
+   public:
+	void init(){
+    for(x=0;x<=32;x+=4)
      for(y=0;y<=64;y+=8)
        {
            point c(x,y);
@@ -15,9 +15,8 @@ class Map{
               c.PrintCanteen();
            else 
               c.PrintResidence();
-       } 
+       }
     }
-    private:
      int x,y;
      int Graph[60][60];//用于记录某个位置是餐厅还是住所 餐厅为1,住所为0
 };

02b25e391891da3a6d8e6572a660ffb57c040c64
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-18 23:00:42 +0800

    Update message.cpp

--- message/message.cpp
+++ message/message.cpp
@@ -5,10 +5,13 @@
 #include"controller.h" 
 void printmessage(struct message a){
 	SetCursorPosition(17,74);
-	printf("当前时刻：%d",sysclock);
-	printf("当前钱数：%d\n",a.messagemoney);
+	printf("当前时刻：%d	",sysclock);
+	SetCursorPosition(18,74);
+	printf("当前钱数：%d\n",money);
+	SetCursorPosition(19,74);
 	printf("接单数：%d\n",a.sum);
+	SetCursorPosition(20,74);
 	printf("完成数：%d\n",a.accomplish);
+	SetCursorPosition(21,74);
 	printf("超时数：%d\n",a.totalovertime);
 }
-

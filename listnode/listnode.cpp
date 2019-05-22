17826391d90ad4e6fef3abe54147c555f3734ad6
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-22 22:57:55 +0800

    Update listnode.cpp

--- listnode/listnode.cpp
+++ listnode/listnode.cpp
@@ -5,7 +5,7 @@ ListNode* ListNode::insertAsPred(const int& x,const int& y,const int& t)
 	ListNode* p = (ListNode*)malloc(sizeof(ListNode)); 
 	p->next = pred->next;
 	pred->next = p;
-	p->next = pred;
+	p->pred = pred;
 	pred = p;
 	return p;
 }

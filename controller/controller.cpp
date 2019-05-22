613956a13def292bb9748715c58fccb5a065caed
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-22 22:55:58 +0800

    Update controller.cpp

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -11,16 +11,20 @@
 //全局变量： 
 int sysclock=0;
 int money=1000;
-struct menu *Menu=(struct menu*)calloc(1,sizeof(struct menu));	//订单动态数组 
-Rider *rider=(Rider*)calloc(2,sizeof(Rider));		//骑手动态数组 
+struct menu Menu[301]={0};	//订单动态数组 
+Rider rider[100];		//骑手动态数组 
 int Graph[100][100];
 void start()
 {
 	SetWindowSize(140,120);			//原图为78*51 
 	//限定骑手起始位置
+	rider[0].receive =0;
+	rider[0].achieve=0;
+	rider[0].overtime=0;
+	rider[0].x=15;
+	rider[0].y=34;
 	rider[0].exist=1;
-	rider[1].exist=0;
-	rider[0].changeposi(15,34);		//x,y具体待补充 ,rider[i].A.changeposi(x,y);
+	rider[1].exist=0;		//x,y具体待补充 ,rider[i].A.changeposi(x,y);
 	int boolnumber=0;				//判断是否破产使用 
 	int j=0;			
 	int number=0;		//读文件中的序号 
@@ -55,7 +59,6 @@ void start()
 				break;										//此处改动 
 			} 
 			if(size!=0){
-				Menu=(struct menu*)realloc(Menu,(size+1)*sizeof(struct menu));	//将订单数组的大小加一 
 				Menu[size].x1=0;						//初始化新增加的订单 
 				Menu[size].y1=0;
 				Menu[size].x2=0;
@@ -91,7 +94,7 @@ void start()
 		//5、判断是否到达送餐点，判断是否超时,采用遍历订单的方法 
 		for(j=0;j<size;j++){				
 			if(((Menu[j].x1-2)==(Menu[j].p)->x&&Menu[j].y1==(Menu[j].p)->y)||(Menu[j].x1==(Menu[j].p)->x&&(Menu[j].y1+4)==(Menu[j].p)->y)||(Menu[j].x1==(Menu[j].p)->x&&(Menu[j].y1-4)==(Menu[j].p)->y)||((Menu[j].x1+2)==(Menu[j].p)->x&&(Menu[j].y1)==(Menu[j].p)->y)&&Menu[j].get==0){ //骑手到达接餐地 
-				Menu[j].get=1;
+				Menu[j].trueget=1;
 			}
 			if(((Menu[j].x2-2)==(Menu[j].p)->x&&Menu[j].y2==(Menu[j].p)->y)||(Menu[j].x2==(Menu[j].p)->x&&(Menu[j].y2+4)==(Menu[j].p)->y)||(Menu[j].x2==(Menu[j].p)->x&&(Menu[j].y2-4)==(Menu[j].p)->y)||((Menu[j].x2+2)==(Menu[j].p)->x&&(Menu[j].y2)==(Menu[j].p)->y)&&Menu[j].get==1){	//骑手到达送餐地 
 				if(sysclock-Menu[j].endtime>=60){		//破产 
@@ -103,14 +106,14 @@ void start()
 				else if(sysclock-Menu[j].endtime>=30){	//超时罚款50,超时数+1 
 					money-=50;
 					Message.totalovertime+=1;
-					Menu[j].reach=1;
+					Menu[j].truereach=1;
 					Menu[j].p->overtime+=1;
 			 		deletelist(j);
 				}
 				else{						//送餐成功，钱数加10,完成数+1 
 					money+=10;
 					Message.accomplish+=1;
-					Menu[j].reach=1;
+					Menu[j].truereach=1;
 					Menu[j].p->achieve+=1;
 					deletelist(j);
 				} 	

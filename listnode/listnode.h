297f1c9051b817a0043a9f0c076680fb6f29dc67
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-22 16:41:19 +0800

    Update listnode.h

--- listnode/listnode.h
+++ listnode/listnode.h
@@ -11,7 +11,6 @@ class ListNode{
     ListNode(int x,int y,int t,ListNode* p=NULL,ListNode* n=NULL): x(x),y(y),t(t),pred(p),next(n) {}//???ˉ * 
 
      ListNode* insertAsPred(const int& x,const int& y,const int& t);
-	 ListNode* insertAsSucc(const int& x,const int& y,const int& t); 
 }; 
 
 

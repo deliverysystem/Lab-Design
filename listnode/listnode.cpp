f95c8391e4b959fc501a824a6943a21480801db4
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-22 16:41:55 +0800

    Update listnode.cpp

--- listnode/listnode.cpp
+++ listnode/listnode.cpp
@@ -1,17 +1,16 @@
 #include "listnode.h"
-
+#include <stdlib.h>
 ListNode* ListNode::insertAsPred(const int& x,const int& y,const int& t)
 {
-	ListNode *p = new ListNode(x,y,t, pred, this);    //更新4个指针的指向
-	pred->next = p;
-	pred = p;
-	return p;
-}//插入一个新节点，作为当前节点的前驱，调用方法  node->insertAsPred(x,y,t)
-
-ListNode* ListNode::insertAsSucc(const int& x,const int& y,const int& t)
-{
-	ListNode *p = new ListNode(x,y,t, this, next);
-	next->next = p;
-	next = p;
-	return p;
-}//插入一个新节点，作为当前节点的后继，调用方法  node->insertAsSucc(x,y,t)
+	ListNode *s = (ListNode*)malloc(sizeof(ListNode));    //鏇存柊4涓寚閽堢殑鎸囧悜
+	s->pred=NULL;
+	s->next=NULL;
+	s->x=x;
+	s->y=y;
+	s->t=t;
+	s->next = pred->next;
+	pred->next = s;
+	s->pred = pred;
+	pred = s; 
+	return s;
+}

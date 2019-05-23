2883b3fc287b4fe89efd4012f82b4230efd5a938
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-23 16:23:40 +0800

    Update message.cpp

--- message/message.cpp
+++ message/message.cpp
@@ -4,6 +4,14 @@
 #include"message.h"
 #include"controller.h" 
 void printmessage(struct message a){
+	int i;
+	int j;
+	for(i=17;i<=21;i++)
+		for(j=74;j<=94;j++)
+		{
+			SetCursorPosition(i,j);
+			printf(" ");
+		}
 	SetCursorPosition(17,74);
 	printf("当前时刻：%d	",sysclock);
 	SetCursorPosition(18,74);
@@ -14,4 +22,5 @@ void printmessage(struct message a){
 	printf("完成数：%d\n",a.accomplish);
 	SetCursorPosition(21,74);
 	printf("超时数：%d\n",a.totalovertime);
+	SetCursorPosition(16,74);
 }

3298bdf907688667f51328dd0bfb77b13c588c84
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-24 22:27:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include"controller.h"
-
+#include"tools.h"
+#include <windows.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	start();
-	SetCursorPosition(50,0);
+	SetCursorPosition(40,0);
 	if(money>=0)
+	{
+		SetColor(FOREGROUND_GREEN);
 		printf("骑手已送完所有订单，程序结束运行\n");
-	else
+	}
+	else{
+		SetColor(FOREGROUND_RED);
 		printf("破产"); 
-	SetCursorPosition(55,0);
+	}
+	SetCursorPosition(45,0);
+	SetColor(FOREGROUND_INTENSITY);
 	return 0;
 }

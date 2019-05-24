1bf3c0fe29385d9b96fb909738bce08b94988d33
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-24 20:53:03 +0800

    Update controller.cpp

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -115,10 +115,10 @@ void start()
 			Menu[size].y1=4*Menu[size].y1+2;
 			Menu[size].x2=2*Menu[size].x2+1;
 			Menu[size].y2=4*Menu[size].y2+2;
-			SetCursorPosition(Menu[size].x1,Menu[size].y1);
-			printf("餐");
-			SetCursorPosition(Menu[size].x2,Menu[size].y2); 
-			printf("客");			
+			SetCursorPosition(Menu[size].x1,Menu[size].y1-1);
+			printf("KFC");
+			SetCursorPosition(Menu[size].x2,Menu[size].y2-1); 
+			printf("BYR");			
 			size++;											
 			Message.sum+=1;					//接单数+1	 
 			//4、分配订单 
@@ -143,8 +143,8 @@ void start()
 				Stop[o].ridern = Menu[j].underline;
 				Stop[o].iscan =1;
 				o++; 
-				SetCursorPosition(Menu[j].x1,Menu[j].y1);
-				printf("  ");
+				SetCursorPosition(Menu[j].x1,Menu[j].y1-1);
+				printf("   ");
 			}
 			if((((Menu[j].x2-2)==(Menu[j].p)->x&&Menu[j].y2==(Menu[j].p)->y)||(Menu[j].x2==(Menu[j].p)->x&&(Menu[j].y2+4)==(Menu[j].p)->y)||(Menu[j].x2==(Menu[j].p)->x&&(Menu[j].y2-4)==(Menu[j].p)->y)||((Menu[j].x2+2)==(Menu[j].p)->x&&(Menu[j].y2)==(Menu[j].p)->y))&&(Menu[j].trueget==1)&&(Menu[j].truereach!=1))	//骑手到达送餐地 
 			{						//送餐成功，钱数加10,完成数+1 
@@ -172,8 +172,8 @@ void start()
 					Stop[o].name=2; //2是食客
 					o++;
 				} 
-				SetCursorPosition(Menu[j].x2,Menu[j].y2);
-				printf("  ");
+				SetCursorPosition(Menu[j].x2,Menu[j].y2-1);
+				printf("   ");
 				Message.accomplish+=1;
 				Menu[j].truereach=1;
 				Menu[j].p->achieve+=1;
@@ -182,25 +182,25 @@ void start()
 				deletelist(j);
 			} 
 		}
-		//防止消除其他单的餐客
+		//6、防止消除其他单的餐客
 		for(j=0;j<size;j++)
 		{
 			if(Menu[j].trueget!=1){
-				SetCursorPosition(Menu[j].x1,Menu[j].y1);
-				printf("餐");
+				SetCursorPosition(Menu[j].x1,Menu[j].y1-1);
+				printf("KFC");
 			}
 			if(Menu[j].truereach!=1){
-				SetCursorPosition(Menu[j].x2,Menu[j].y2);
-				printf("客");
+				SetCursorPosition(Menu[j].x2,Menu[j].y2-1);
+				printf("BYR");
 			}
 		} 
-		//将信息输入到文件中
+		//7、将信息输入到文件中
 		fprintf(fp,"时间：%d\n",sysclock);
 		fprintf(fp,"钱：%d\n",money);
 		fprintf(fp,"接单数：%d\n",Message.sum);
 		fprintf(fp,"完成数：%d;结单：",Message.accomplish);
 		for(i=0;rightnowfinish[i]!=0;i++){
-			if(rightnowticket[i+1]==0){
+			if(rightnowfinish[i+1]==0){
 				fprintf(fp,"%d ",rightnowfinish[i]);
 				fprintf(fp," ");
 			}
@@ -224,15 +224,17 @@ void start()
 		fprintf(fp,";\n");
 		for(j=0;rider[j].exist==1;j++){
 			fprintf(fp,"骑手%d的位置: %d, %d; 停靠:",j+1,(rider[j].x-1)/2,(rider[j].y-2)/4);//2*rider[i].x+1,4*rider[i].y+2
-		for(i=0;Stop[i].ridern==j&&Stop[i].flag!=0;i++){
-			if(Stop[i].name==1)
+		for(i=0;Stop[i].flag!=0;i++){
+			if(Stop[i].ridern==j){
+				if(Stop[i].name==1)
 				fprintf(fp,"餐馆 "); 
 			else if(Stop[i].name==2)
 				fprintf(fp,"食客 "); 
 			else
 				fprintf(fp,"餐客 ");
 			fprintf(fp,"%d %d ",(Stop[i].x-1)/2,(Stop[i].y-2)/4);
-		}
+				}
+			}
 			fprintf(fp,";\n");
 		} 	
 		//6、对骑手进行移动 

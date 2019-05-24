055bf5496e2e9dd5141003e3ea79758143281c86
wanderingjunzai <49681708+wanderingjunzai@users.noreply.github.com>
2019-05-24 22:31:06 +0800

    Update message.cpp

--- message/message.cpp
+++ message/message.cpp
@@ -6,21 +6,44 @@
 void printmessage(struct message a){
 	int i;
 	int j;
-	for(i=17;i<=21;i++)
-		for(j=74;j<=94;j++)
-		{
-			SetCursorPosition(i,j);
-			printf(" ");
-		}
-	SetCursorPosition(17,74);
+	for(i=28;i<=32;i++){
+		int j=76; 
+		SetCursorPosition(i,j);
+		printf("                                        ");
+	}
+	SetCursorPosition(27,76);
+	printf("————————————");
+	SetCursorPosition(33,76);
+	printf("————————————");
+	SetCursorPosition(28,76);
+	printf("|");
+	SetCursorPosition(29,76);
+	printf("|");
+	SetCursorPosition(30,76);
+	printf("|");
+	SetCursorPosition(31,76);
+	printf("|");
+	SetCursorPosition(32,76);
+	printf("|");
+	SetCursorPosition(28,99);
+	printf("|");
+	SetCursorPosition(29,99);
+	printf("|");
+	SetCursorPosition(30,99);
+	printf("|");
+	SetCursorPosition(31,99);
+	printf("|");
+	SetCursorPosition(32,99);
+	printf("|");
+	SetCursorPosition(28,78);
 	printf("当前时刻：%d	",sysclock);
-	SetCursorPosition(18,74);
+	SetCursorPosition(29,78);
 	printf("当前钱数：%d\n",money);
-	SetCursorPosition(19,74);
+	SetCursorPosition(30,78);
 	printf("接单数：%d\n",a.sum);
-	SetCursorPosition(20,74);
+	SetCursorPosition(31,78);
 	printf("完成数：%d\n",a.accomplish);
-	SetCursorPosition(21,74);
+	SetCursorPosition(32,78);
 	printf("超时数：%d\n",a.totalovertime);
-	SetCursorPosition(16,74);
+	SetCursorPosition(26,87);
 }

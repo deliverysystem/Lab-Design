5a66adc31b2d717f1b6027d666c9cc16ba838252
Ghiad <48151874+Ghiad@users.noreply.github.com>
2019-06-05 19:36:30 +0800

    动画

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include"controller.h"
+#include"cartoon.h"
 #include"tools.h"
 #include <windows.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include<stdio.h>
+#include <process.h> 
+
 
 int main(int argc, char** argv) {
-	start();
-	SetCursorPosition(40,0);
+	setinitmode(INIT_ANIMATION);//窗口初始化 
+	initgraph(1100, 720);
+	HANDLE hThread1,hThread2;//线程部分 
+	hMutex=CreateMutex(NULL,FALSE,NULL);
+   	hThread1=(HANDLE)_beginthreadex(NULL,0,getinput,NULL,0,NULL);   
+	hThread2=(HANDLE)_beginthreadex(NULL,0,start,NULL,0,NULL);
+    WaitForSingleObject(hThread1,INFINITE);	
+	CloseHandle(hThread1);
+    WaitForSingleObject(hThread2,INFINITE);	
+	CloseHandle(hThread2);
+	
+	
+	
+	/*SetCursorPosition(40,0);
 	if(money>=0)
 	{
 		SetColor(FOREGROUND_GREEN);
@@ -17,6 +32,7 @@ int main(int argc, char** argv) {
 		printf("破产"); 
 	}
 	SetCursorPosition(45,0);
-	SetColor(FOREGROUND_INTENSITY);
+	SetColor(FOREGROUND_INTENSITY);*/
+	closegraph();
 	return 0;
 }

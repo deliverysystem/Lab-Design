5ede8ee3c76a3d0d850870361e86ddb8336cc78b
Ghiad <48151874+Ghiad@users.noreply.github.com>
2019-06-16 15:12:06 +0800

    鼠标

--- cartoon.cpp
+++ cartoon.cpp
@@ -14,7 +14,7 @@ void printcartoonmessage(struct message a){
 	xyprintf(900,160,"%d",sysclock);
 	xyprintf(945,610,"%d",money);
 	xyprintf(920,290,"%d/%d",a.accomplish,a.sum);
-	xyprintf(850,440,"%d",a.totalovertime);
+	xyprintf(870,440,"%d",a.totalovertime);
 }
 void initimg(){//对骑手的IMG初始化 
 	int i;
@@ -43,7 +43,7 @@ void carprint(int x,int y,int i){
 	if(((y-2)/4*41)%2==0){				//如果骑手在横轴上 
 		//cleardevice(rider[i].Riderimg);
 		getimage(rider[i].Riderimg,"C:/Users/ASUS/Desktop/9.jpg");
-		int pwidth1 =57,pheight1=23;
+		/*int pwidth1 =57,pheight1=23;
 		//1.照片缩放 
 		PIMAGE save1=gettarget();
 		//Get image size
@@ -57,12 +57,12 @@ void carprint(int x,int y,int i){
 		putimage(backproc1,0,0,pwidth1,pheight1,rider[i].Riderimg,0,0,width1,height1); 
 		getimage(rider[i].Riderimg,backproc1,0,0,pwidth1,pheight1);
 		delimage(backproc1);
-		//2.打印骑手 
+		//2.打印骑手 */
 		putimage((y-2)/4*41,(x-1)/2*41+17,rider[i].Riderimg);
 	}
 	else{
-		getimage(rider[i].Riderimg,"C:/Users/ASUS/Desktop/8.jpg");
-		int pwidth1 =23,pheight1=57;
+		getimage(rider[i].Riderimg,"C:/Users/ASUS/Desktop/7.jpg");
+		/*int pwidth1 =23,pheight1=57;
 		
 		PIMAGE save1=gettarget();
 		//Get image size
@@ -75,7 +75,7 @@ void carprint(int x,int y,int i){
 		//Resize
 		putimage(backproc1,0,0,pwidth1,pheight1,rider[i].Riderimg,0,0,width1,height1); 
 		getimage(rider[i].Riderimg,backproc1,0,0,pwidth1,pheight1);
-		delimage(backproc1);
+		delimage(backproc1);*/
 		
 		putimage((y-2)/4*41+17,(x-1)/2*41,rider[i].Riderimg);
 	}
@@ -90,7 +90,7 @@ void printmove()
 	for(i=0;rider[i].exist==1;i++){
 		if(rider[i].Path.header->next!=rider[i].Path.tailer){
 			a.changeposi(rider[i].x,rider[i].y);
-			a.clear();
+			//a.clear();
 			//clearrider(a.x,a.y,i);							//清除骑手当前位置的图像 
 			m=rider[i].Path.header->next->x;				//rider[i]的行驶路线中的下个一点，读出它的坐标 
 			n=rider[i].Path.header->next->y;
@@ -104,13 +104,40 @@ void printmove()
 	}
 	for(i=0;rider[i].exist==1;i++)
 	{
-		SetColor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN);
+		//SetColor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN);
 		a.changeposi(rider[i].x,rider[i].y);
-		a.PrintRider();
+		//a.PrintRider();
 		carprint(a.x,a.y,i);
-		SetColor(FOREGROUND_INTENSITY);
+		//SetColor(FOREGROUND_INTENSITY);
 	}
 }
+
+void printorder(){
+	int i;
+	PIMAGE img;
+	img = newimage();
+	getimage(img,"C:/Users/ASUS/Desktop/12.jpg"); 
+	/*int pwidth = 57, pheight = 57;
+	PIMAGE save=gettarget();
+	//Get image size
+	settarget(img); 
+	int width,height;
+	width=getwidth();
+	height=getheight();
+	settarget(save); 
+	PIMAGE backproc=newimage(pwidth,pheight);
+	//Resize
+	putimage(backproc,0,0,pwidth,pheight,img,0,0,width,height); 
+	getimage(img,backproc,0,0,pwidth,pheight);
+	delimage(backproc);*/
+		
+	for(i=0;Menu[i].x1!=0;i++){
+		if(Menu[i].trueget==0)//没取 
+			putimage((Menu[i].y1-2)/4*41,(Menu[i].x1-1)/2*41,img);
+		if(Menu[i].truereach==0&&Menu[i].mouseup==1)//没到 
+			putimage((Menu[i].y2-2)/4*41,(Menu[i].x2-1)/2*41,img);
+	}
+} 
 unsigned __stdcall getinput(void* pArguments){
 	WaitForSingleObject(hMutex,INFINITE);
 	int flag1,flag2,i=0,j=0;//flag1,2,是用来判断订单输入的 
@@ -127,27 +154,11 @@ unsigned __stdcall getinput(void* pArguments){
 	rider[0].y=34;
 	rider[0].exist=1;
 	rider[1].exist=0;		//x,y具体待补充 ,rider[i].A.changeposi(x,y);
-	for(;is_run();delay_fps(60)){
+	for(;is_run();delay_fps(80)){
 		
-		
-		/*static PIMAGE page = NULL;//绘图层2，用于刷新画面,好像没用 
-		if (page == NULL) {
-		page = newimage(1100, 720);
-		setbkcolor(WHITE);
-		}
-		cleardevice(page);
-		settarget(page);
-		setbkcolor(0xFCFCFC);
-		settarget(NULL);*/
-		//2.买骑手，不知道为啥就只买了一个骑手，但只剩100块钱,在调用buyrider前钱数为700，调用完为100，说明买了但骑手不知为何只有1。。。 
+		//2.买骑手
 		 
 		buyrider();	
-		/*j=0;        //测试现在有多少骑手 
-		for(i=0;rider[i].exist ==1;i++){
-			j++;
-		} 
-		xyprintf(720,50,"钱数=%d",money);
-		xyprintf(720,100,"骑手数量=%d",j);*/
 		
 		if(initflag==1){//只初始化一次 
 			initimg();
@@ -162,15 +173,15 @@ unsigned __stdcall getinput(void* pArguments){
 		
 		flag1=(int)msg.is_down();
 		flag2=(int)msg.is_up();
-		if(flag1!=oldflag1&&flag1==1&&(msg.y/27/3)%2==0&&(msg.x/27/3)%2==0){//如果鼠标点下 且是房子 
-			Menu[size].y1=msg.x/27/3;//动画的xy与控制台的相反 
-			Menu[size].x1=msg.y/27/3;
-			Menu[size].x1=4*Menu[size].x1+2;
-			Menu[size].y1=2*Menu[size].y1+1;	
-			outtextxy((msg.x/27/3*2+msg.x/27/3)*27,(msg.y/27/3*2+msg.y/27/3)*27,"下单");//后期可以改为贴图 
+		if(flag1!=oldflag1&&flag1==1){//如果鼠标点下 且是房子 
+			Menu[size].y1=msg.x/27/3*2;//动画的xy与控制台的相反 
+			Menu[size].x1=msg.y/27/3*2;
+			Menu[size].x1=2*Menu[size].x1+1;
+			Menu[size].y1=4*Menu[size].y1+2;	
+			//outtextxy((msg.x/27/3*2+msg.x/27/3)*27,(msg.y/27/3*2+msg.y/27/3)*27,"下单");//后期可以改为贴图 
 		}
 		oldflag1=flag1;
-		if(flag2==1&&flag2!=oldflag2&&(msg.y/27/3)%2==0&&(msg.x/27/3)%2==0){//与上面相同 
+		if(flag2==1&&flag2!=oldflag2){//与上面相同 
 			Menu[size].p=NULL;
 			Menu[size].get=0;
 			Menu[size].reach=0;
@@ -178,58 +189,22 @@ unsigned __stdcall getinput(void* pArguments){
 			Menu[size].truereach=0;
 			Menu[size].trueget=0;
 			Menu[size].trueovertime=0;
-			Menu[size].x2=msg.y/27/3;
-			Menu[size].y2=msg.x/27/3;
-			Menu[size].x2=4*Menu[size].x2+2;
-			Menu[size].y2=2*Menu[size].y2+1;	
+			Menu[size].mouseup=1;
+			Menu[size].x2=msg.y/27/3*2;
+			Menu[size].y2=msg.x/27/3*2;
+			Menu[size].x2=2*Menu[size].x2+1;
+			Menu[size].y2=4*Menu[size].y2+2;	
 			Menu[size].endtime=sysclock+30;
 			size++;
+			Message.sum ++;
 			allocatemenu(size-1);//size-1为此刻新接订单在订单数组中的下标 
-			outtextxy((msg.x/27/3*2+msg.x/27/3)*27,(msg.y/27/3*2+msg.y/27/3)*27,"下单");
+			//outtextxy((msg.x/27/3*2+msg.x/27/3)*27,(msg.y/27/3*2+msg.y/27/3)*27,"下单");
 		}
 		oldflag2=flag2;
 		
 		
 	
-		//2.打印地图 ，地图只打印一次 
-		PIMAGE img=newimage();
-		getimage(img,"C:/Users/ASUS/Desktop/map2.jpg"); 
-		
-		int pwidth = 1100, pheight = 720;
-		//Resize image to pwidth*pheight
-		PIMAGE save=gettarget();
-		//Get image size
-		settarget(img); 
-		int width,height;
-		width=getwidth();
-		height=getheight();
-		settarget(save); 
-		PIMAGE backproc=newimage(pwidth,pheight);
-		//Resize
-		putimage(backproc,0,0,pwidth,pheight,img,0,0,width,height); 
-		getimage(img,backproc,0,0,pwidth,pheight);
-		delimage(backproc);
-		
-		putimage(0,0,img);
-		//3.打印骑手 ,等文件输入没问题后，删除文件输入的相关部分，再调用这个部分 
-		if(size!=0)							
-			printmove();                                       
-		else{
-			SetColor(FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN);
-			point a(15,34);
-			a.PrintRider();
-			carprint(a.x,a.y,0);
-			SetColor(FOREGROUND_INTENSITY);	
-		}
-		
-		xyprintf(0,50,"SIZE = %d",size);
-		char str[20];
-		sprintf(str, "fps %.02f", getfps()); //调用getfps取得当前帧率
-		setcolor(WHITE);
-		outtextxy(0, 0, str);
-		
-		printcartoonmessage(Message);
-		Sleep(1000);
+		Sleep(150);
 		ReleaseMutex(hMutex);
 	}
 	release();

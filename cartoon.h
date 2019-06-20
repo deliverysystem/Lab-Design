246024cef53a04c1e86ff9e5102d5eed3cbc3231
linln1 <41333235+linln1@users.noreply.github.com>
2019-06-20 19:40:03 +0800

    Update cartoon.h

--- cartoon.h
+++ cartoon.h
@@ -5,8 +5,9 @@ unsigned __stdcall getinput(void* pArguments);
 extern mouse_msg msg ;
 void carprint(int ,int,int);
 extern void printmove();//对骑手进行移动 
-void initimg();//对骑手的IMG初始化 
+void initimg(int );//对骑手的IMG初始化 
 void clearrider(int ,int ,int );//清除骑手上一个位置的图像，有问题，在一些点会有残影 
 void release(); //程序结束时释放骑手的IMG 
 void printcartoonmessage(struct message);
+void printorder();
 #endif
